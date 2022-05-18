1370cdaa324a61291dcfd45e6fceec2899e0cddf
chl9430 <chl9430@naver.com>
2022-05-18 16:24:36 +0900

    fix: Fixed address!
    I fixed address which includes articles only related "Apple".

--- week6/jung-hoo/main.js
+++ week6/jung-hoo/main.js
@@ -3,19 +3,32 @@ const buttonEl = document.querySelector(".search-button");
 const resBoxEl = document.querySelector(".result-box");
 const filterEl = document.querySelector(".search-filter");
 
-fetch(
-  "https://newsapi.org/v2/everything?q=Apple&from=2022-05-16&sortBy=popularity&apiKey=2c157850e4c64fd99aaf77d60228d630"
-)
-  .then((response) => response.json())
-  .then((data) => {
-    buttonEl.addEventListener("click", doSearch); // 버튼에 이벤트 추가한다.
+buttonEl.addEventListener("click", doSearch); // 버튼에 이벤트 추가한다.
 
-    function doSearch() {
-      const inputEl = document.querySelector(".search-input");
-      resBoxEl.innerHTML = "";
+function doSearch() {
+  // 버튼을 클릭하면...
+  const inputEl = document.querySelector(".search-input"); // input을 가져온다.
+  if (inputEl.value === "") {
+    alert("검색어를 입력하세요!");
+    return;
+  }
+  resBoxEl.innerHTML = ""; // 결과를 탐색하기 전 결과물을 다 지운다.
+
+  fetch(
+    `https://newsapi.org/v2/everything?q=${inputEl.value}&sortBy=popularity&apiKey=2c157850e4c64fd99aaf77d60228d630`
+  )
+    .then(function (res) {
+      return res.json();
+    })
+    .then(function (data) {
+      console.log(data);
+      if (data.articles.length === 0) {
+        // 아예 기사를 못가져왔을 경우
+        alert("검색결과가 없습니다!"); // 경고창을 띄운다.
+      }
 
       for (let i = 0; i < data.articles.length; ++i) {
-        // 기사의 갯수만큼 반복문으로 비교를 실행한다.
+        // 가져온 기사의 갯수만큼 반복문으로 비교를 실행한다.
         if (filterEl.value === "제목") {
           // 검색 조건이 제목이라면...
           if (
@@ -43,6 +56,13 @@ fetch(
                 <a href="${data.articles[i].url}">See more</a></div>`;
           }
         }
+
+        if (i === data.articles.length - 1) {
+          // 가져온 기사들 중 조건에 부합하는 기사가 없다면...
+          if (resBoxEl.innerHTML === "") {
+            alert("검색결과가 없습니다!"); // 경고창을 띄운다.
+          }
+        }
       }
-    }
-  });
+    });
+}
